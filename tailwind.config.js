c656eadb4d1ca1c475c33418e61b6853c16fbd75
molla mehedi <mollameehedi@gmail.com>
2023-11-27 19:23:45 +0600

    initial Project folder

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      lineHeight: {
+        'nav': '100px',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
