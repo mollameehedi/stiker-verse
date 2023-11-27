0c36bd11f76f5ee137105f41b08be8b0c7feb724
molla mehedi <mollameehedi@gmail.com>
2023-11-27 20:20:25 +0600

    font family add and some common class create

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,20 @@ module.exports = {
     extend: {
       lineHeight: {
         'nav': '100px',
-      }
+      },
+      colors: {
+        'primary': '#ffdb00',
+        'link': '#2b78c5',
+        'secondary': '#444',
+        'btnBg': '#2d2d2d',
+        'navFo': 'rgb(4, 4, 4)',
+        'copyRight': 'rgb(17, 17, 17)',
+      },
     },
+    fontFamily: {
+      'futurapt': ['Futura PT', 'sans-serif'],
+      'helvetica': ['Helvetica Neue', 'sans-serif'],
+    }
   },
   variants: {
     extend: {},
